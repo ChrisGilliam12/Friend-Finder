9e8b3d04f758d0dfda9745b47e8a99381d0aaadd
Chris Gilliam <chrisgilliam123@yahoo.com>
2020-01-21 19:59:49 -0600

    moved files to new repo to fix linking issues

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friendsList = [
+	{
+	  name: "Bob",
+	  photo: "https:www.Google.com",
+	  preferences: [1, 2, 3, 4, 5,]
+    },
+    {
+        name: "Mike",
+        photo: "https:www.Google.com",
+        preferences: [2, 3, 4, 5, 1]
+      },
+      {
+        name: "Alice",
+        photo: "https:www.Google.com",
+        preferences: [3, 4, 5, 1, 2]
+      },
+      {
+        name: "Greg",
+        photo: "https:www.Google.com",
+        preferences: [4, 5, 1, 2, 3]
+      },
+      {
+        name: "Jill",
+        photo: "https:www.Google.com",
+        preferences: [5, 1, 2, 3, 4]
+      },
+
+
+];
+
+module.exports = friendsList;
